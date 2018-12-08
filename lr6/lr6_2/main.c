4f61103953867dc2534bb052eb0c99d7a5103938
AMaslenkina <43391778+AMaslenkina@users.noreply.github.com>
2018-12-08 15:15:52 +0300

    Add files via upload

---
+++ lr6/lr6_2/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 10
+#define M 12
+int main(void)
+{
+  int *matr;
+  int minr,maxc,i,j,y,x,f=0;
+  srand(time(NULL));
+  for(i=0;i<N;i++)
+  	for(j=0;j<M;j++)
+  		*matr=rand() %(200-(-200))+(-200);
+  		matr = (int*)malloc(N*M * sizeof(int));
+  for(i=0;i<N;i++)
+  {
+      printf("\n");
+      for(j=0;j<M;j++)
+      printf("%d ",(matr+i+j));
+  }
+  printf("\n");
+  for (i=0;i<N;i++) 
+  {
+     minr=(matr+i+0);
+     for (x=1;x<M;x++)
+     {
+        if ( *(matr+i+x)<minr )
+        {
+           minr=*(matr+i+x); 
+        }
+     }
+     for( j=0;j<M;j++)
+     {
+         if ( *(matr+i+j)==minr)
+         {  
+            maxc=*(matr+0+j);
+            for (y=1;y<N;y++) 
+            {
+                if ( *(matr+y+j)>maxc ) 
+                { 
+                    maxc= *(matr+y+j);
+                }
+            }
+                   if (*(matr+i+j)==maxc)
+                   { 
+                      printf( "matr[%d][%d]=%d\n", i, j, *(matr+i+j) ); 
+                      f=1;
+                   }
+         }
+     }
+  }
+if(!f)printf("Matrica ne imeet sedl. tochek!\n"); 
+return (0);
+}
+
